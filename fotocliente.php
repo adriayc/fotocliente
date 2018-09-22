e336fede0eb9b6c5190ac591b7753eb85aa5811c
Adriano Ayala <adriano.ayc@gmail.com>
2018-09-22 10:03:59 -0400

    Configuracion del modulo - variables de configuracion

--- fotocliente.php
+++ fotocliente.php
@@ -6,6 +6,17 @@
  * Time: 9:35 AM
  */
 
-class FotoCliente {
+class FotoCliente extends Module {
 
+    public function __construct() {
+        $this->name = "fotocliente";    //Nombre interno del modulo
+        $this->displayName = "Fotos de los clientes";   //Nombre del modulo
+        $this->description = "Modulo que sirve para que los clientes puedan añadir sus propias fotos en los productos"; //Descripcion del modulo
+        $this->tab = "front_office_features";   //Seccion donde se mostrara el modulo
+        $this->author = "Adriano Ayala";    //Autor del modulo
+        $this->version = "1.0"; //Version del modulo
+        $this->bootstap = true; //Habilitar bootstrap en el modulo
+
+        parent::__construct();
+    }
 }
