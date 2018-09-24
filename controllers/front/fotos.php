badfae3cb04e794868b94d7288e74a463e42eef8
Adriano Ayala <adriano.ayc@gmail.com>
2018-09-24 00:53:52 -0400

    Que son los front controllers - creando un controlador

---
+++ controllers/front/fotos.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adriano
+ * Date: 9/24/2018
+ * Time: 12:46 AM
+ */
+
+class FotoclienteFotosModuleFrontController extends ModuleFrontController {
+
+    public function initContent() {
+        parent::initContent();
+    }
+}
